9bf8f24084195e1b2dadc8ab8edc53a47a4f59e2
Rasskris <rassoshenko.kristina@yandex.ru>
2021-02-18 12:59:14 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  const res = [];
+
+  for (let char of str) {
+    for (let item of bracketsConfig) {
+      if (char === item[0]) {
+        res.push(char);
+      } 
+      if (char === item[1]) {
+        const lastIndex = res.length - 1;
+        if (res[lastIndex] === item[0]) {
+          if (item[0] === item[1]) {
+            const countItem = res.filter((item) => item === char);
+            if (countItem.length % 2 === 0) {
+              res.splice(-2);
+            }
+            continue;
+          }
+          res.pop();
+        } else {
+          return false;
+        }
+      }
+    }
+  }
+  return res.length === 0;
+};
